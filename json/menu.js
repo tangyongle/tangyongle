c14deb66ce2f98ddd626082d3fe8c8900f99bd19
tangyongle <283818296@qq.com>
2018-11-13 00:47:15 +0800

    Add files via upload

---
+++ json/menu.js
@@ -0,0 +1,85 @@
+{
+    "code": 0,
+	"msg": "",
+	"data": [{
+		"title": "Home"
+		,"icon": "layui-icon-home"
+		,"list": []
+	},{
+	    "name": "component",
+	    "title": "Workflow Management",
+	    "icon": "layui-icon-home-2",
+	    "list": []
+	}, {
+		"name": "organization",
+		"title": "Budget Management",
+		"icon": "layui-icon-home-3",
+		"list": [{
+			"name": "#"
+			,"title": "Budget Allocation"
+		},{
+            "name": "#"
+            ,"title": "Budget Adjustment"
+        }]
+	}, {
+		"name": "newSystem",
+		"title": "Contract Management",
+		"icon": "layui-icon-home-4",
+		"list": [{
+			"name": "#"
+			,"title": "Contract List"
+		},{
+			"name": "#"
+			,"title": "Execution Tracking"
+		}]
+	}, {
+		"name": "photoManagement",
+		"title": "Payment Management",
+		"icon": "layui-icon-home-5",
+		"list": [{
+            "name": "#"
+            ,"title": "Payment Management 1"
+        },{
+            "name": "#"
+            ,"title": "EPayment Management 2"
+        }]
+	}, {
+		"name": "#",
+		"title": "Finace Period End",
+		"icon": "layui-icon-home-6",
+		"list": []
+	}, {
+		"name": "#",
+		"title": "Report",
+		"icon": "layui-icon-home-7",
+		"list": []
+	}, {
+        "name": "#",
+        "title": "Master Data",
+        "icon": "layui-icon-home-8",
+        "list": []
+    }, {
+		"name": "statisticAnalysis",
+		"title": "Administration",
+		"icon": "layui-icon-home-9",
+		"list": [{
+			"name": "#"
+			,"title": "User Profile"
+		},{
+            "name": "#"
+            ,"title": "Inbox"
+        },{
+            "name": "#"
+            ,"title": "Pricing Tables"
+        },{
+            "name": "#"
+            ,"title": "Invoice"
+        },{
+            "name": "#"
+            ,"title": "Timeline"
+        },{
+            "name": "#"
+            ,"title": "Login &amp; Register"
+        }]
+	}]
+}
